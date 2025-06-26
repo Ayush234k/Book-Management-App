70ede829ad815c00fa3daecdfb18420dbc770f09
Ayush232k <a.singh232k@gmail.com>
2025-06-26 12:45:41 +0530

    Backend (Spring-Boot) Initialized

---
+++ bookManagementBackend/src/test/java/com/example/bookManagementBackend/BookManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookManagementBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
